6564deb65cc0fde16198f15821243eac663c65a0
Alexander Tomokhov <alexoundos@gmail.com>
2023-08-16 07:25:37 +0400

    addStandaloneLambdaHandler instead of addAPIGatewayHandler

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE DeriveGeneric #-}
 module Main where
 
 import Aws.Lambda
@@ -7,22 +6,27 @@ import Data.Text
 import qualified Data.Text as Text
 import GHC.Generics
 import Data.Aeson
+import qualified Data.Aeson.KeyMap as KeyMap
+import Data.Maybe
 
 main :: IO ()
 main =
   runLambdaHaskellRuntime defaultDispatcherOptions (pure ()) id $ do
-    addAPIGatewayHandler "gatewayHandler" gatewayHandler
+    addStandaloneLambdaHandler "standaloneHandler" standaloneHandler
 
-gatewayHandler :: ApiGatewayRequest ()
-               -> Context context
-               -> IO (Either (ApiGatewayResponse Text) (ApiGatewayResponse Text))
-gatewayHandler gatewayReq _context = do
-  print gatewayReq
-  pure . Right $ ApiGatewayResponse 200 [] (Text.pack $ show gatewayReq) False
+standaloneHandler :: Value -> Context () -> IO (Either String Text)
+standaloneHandler req _context = do
+  print req -- this gets printed in CloudWatch logs
+  pure $ case parseParam req of
+    Nothing -> error "no \"param\" found" -- (Left values are not printed)
+    Just paramTxt -> Right paramTxt
 
-data TestResponse = TestResponse {
-  testResponse :: Text
-} deriving (Eq, Ord, Show, Generic)
-
-instance ToJSON TestResponse
-instance FromJSON TestResponse
+parseParam :: Value -> Maybe Text
+parseParam (Object o) =
+  case KeyMap.lookup "queryStringParameters" o of
+    Just (Object queryStringParameters) ->
+      case KeyMap.lookup "param" queryStringParameters of
+        Just (String paramTxt) -> Just paramTxt
+        _ -> Nothing
+    _ -> Nothing
+parseParam _ = Nothing

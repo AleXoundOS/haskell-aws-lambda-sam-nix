0804c9af9944a502e52d809834be7af4186ba295
Alexander Tomokhov <alexoundos@gmail.com>
2023-08-16 07:25:37 +0400

    make-aws-lambda-zip.nix creates zip archive with de-nixified binaries

--- flake.nix
+++ flake.nix
@@ -5,34 +5,35 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
   outputs = { self, nixpkgs, flake-utils, haskellNix }:
     flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
-    let
-      overlays = [ haskellNix.overlay
-        (final: prev: {
-          # This overlay adds our project to pkgs
-          helloProject =
-            final.haskell-nix.project' {
-              src = ./.;
-              compiler-nix-name = "ghc928";
-              # This is used by `nix develop .` to open a shell for use with
-              # `cabal`, `hlint` and `haskell-language-server`
-              shell.tools = {
-                cabal = {};
-                hlint = {};
-                haskell-language-server = {};
+      let
+        overlays = [
+          haskellNix.overlay
+          (final: prev: {
+            # This overlay adds our project to pkgs
+            helloProject =
+              final.haskell-nix.project' {
+                src = ./.;
+                compiler-nix-name = "ghc928";
+                # This is used by `nix develop .` to open a shell for use with
+                # `cabal`, `hlint` and `haskell-language-server`
+                shell.tools = {
+                  cabal = { };
+                  hlint = { };
+                  haskell-language-server = { };
+                };
+                # Non-Haskell shell tools go here
+                shell.buildInputs = with pkgs; [
+                  nixpkgs-fmt
+                ];
               };
-              # Non-Haskell shell tools go here
-              shell.buildInputs = with pkgs; [
-                nixpkgs-fmt
-              ];
-            };
-        })
-      ];
-      pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
-      flake = pkgs.helloProject.flake {
-
-      };
-    in flake // {
-      # Built by `nix build .`
-      defaultPackage = flake.packages."haskell-aws-lambda-sam-nix:exe:haskell-aws-lambda-sam-nix";
-    });
+          })
+        ];
+        pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
+        flake = pkgs.helloProject.flake { };
+      in
+      flake // {
+        # Built by `nix build .`
+        defaultPackage = flake.packages."haskell-aws-lambda-sam-nix:exe:haskell-aws-lambda-sam-nix";
+        aws-lambda = import ./make-aws-lambda-zip.nix { inherit pkgs flake; };
+      });
 }
---
+++ make-aws-lambda-zip.nix
@@ -0,0 +1,33 @@
+{ pkgs, flake }:
+
+let
+  buildLambda = cabalProject: executable:
+    let
+      exeComponent = flake.packages."${cabalProject}:exe:${executable}";
+    in
+      pkgs.runCommand executable { buildInputs = with pkgs; [ exeComponent glibc.bin patchelf zip ]; } ''
+        # Copy the binary to `bootstrap`, which is what AWS Lambda expects:
+        # https://docs.aws.amazon.com/lambda/latest/dg/runtimes-custom.html
+        cp ${exeComponent}/bin/${executable} bootstrap
+
+        # Copy the shared objects that our binary depends on to a subfolder `lib/`.
+        mkdir lib
+        cp $(ldd bootstrap | grep -F '=> /' | awk '{print $3}') lib/
+
+        # Patch the binary to point the ELF interpreter and the run-time search
+        # path to the shared objects we provide. Note that these paths are location
+        # independent: as long as the binary is in the same directory as the folder
+        # containing our shared objects, this will work.
+        chmod +w bootstrap
+        patchelf --set-interpreter ./lib/ld-linux-x86-64.so.2 --set-rpath ./lib --force-rpath bootstrap
+        chmod -w bootstrap
+
+        # Finally, we can zip up our binary and the subfolder holding our shared objects.
+        # This zip file is the output artefact of this derivation and can be uploaded to AWS
+        # Lambda as-is.
+        mkdir $out
+        zip -qr $out/${executable}.zip lib bootstrap
+      '';
+in
+  buildLambda "haskell-aws-lambda-sam-nix" "haskell-aws-lambda-sam-nix"
+

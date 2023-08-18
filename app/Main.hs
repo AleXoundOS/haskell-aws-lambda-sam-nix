4f58999bc5de31d7f9be8ff702ae906d42407a26
Alexander Tomokhov <alexoundos@gmail.com>
2023-08-18 05:13:45 +0400

    a test query to retrive names of all DynamoDB tables in a current region

--- app/Main.hs
+++ app/Main.hs
@@ -1,26 +1,67 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Aws.Lambda
+import Aws.Lambda as Aws
+import qualified Amazonka as A
+import Amazonka.DynamoDB as DynamoDB
+import Amazonka.DynamoDB.ListTables as DynamoDB
 import Data.Text
+import qualified Data.Text.IO as Text
 import qualified Data.Text as Text
 import Data.Aeson
+import Data.Conduit
+import qualified Data.Conduit.Combinators as CC
+import qualified Data.Conduit.List as CL
 import qualified Data.Aeson.KeyMap as KeyMap
+import Control.Monad.IO.Class
 import Data.IORef
 import Data.Time.Clock (getCurrentTime, UTCTime)
+import System.IO (stdout)
+
+initializeContext :: IO (UTCTime, A.Env)
+initializeContext = do
+  -- note that CloudWatch logging in AWS expensive and takes lots of Lambda time
+  logger <- A.newLogger A.Debug stdout
+  env <- setLoggerInAwsEnv logger <$> A.newEnv A.discover
+  curTime <- getCurrentTime
+  pure (curTime, env)
+  where
+    setLoggerInAwsEnv :: A.Logger -> A.Env -> A.Env
+    setLoggerInAwsEnv logger env = env {A.logger = logger}
 
 main :: IO ()
-main =
-  runLambdaHaskellRuntime defaultDispatcherOptions getCurrentTime id $ do
+main = do
+  runLambdaHaskellRuntime defaultDispatcherOptions initializeContext id $ do
     addStandaloneLambdaHandler "standaloneHandler" standaloneHandler
 
-standaloneHandler :: Value -> Context UTCTime -> IO (Either String Text)
+standaloneHandler :: Value -> Aws.Context (UTCTime, A.Env)
+                  -> IO (Either String Text)
 standaloneHandler req awsContext = do
-  context <- readIORef $ customContext awsContext
-  print req -- this gets printed in CloudWatch logs
+  print req -- this is printed in CloudWatch logs at the cost of time
+  (initTime, env) <- readIORef $ customContext awsContext
+  putStrLn $ "running in region: " ++ show (A.region env)
+  tables <- getTables env
+
   pure $ case parseParam req of
     Nothing -> error "no \"param\" found" -- (Left values are not printed)
-    Just paramTxt -> Right (Text.pack (show context) <> "\n" <> paramTxt)
+    Just paramTxt -> Right (text env paramTxt initTime tables)
+  where
+    text env paramTxt initTime tables = ""
+      <> "running in region: " <> Text.pack (show $ A.region env) <> "\n"
+      <> "init time: " <> Text.pack (show initTime) <> "\n"
+      <> "param value: " <> paramTxt <> "\n"
+      <> "existing tables in region: " <> Text.intercalate ", " tables
+
+getTables :: A.Env -> IO [Text]
+getTables env = do
+  A.runResourceT $ do
+    runConduit $
+      A.paginate env newListTables
+        .| CL.map DynamoDB.tableNames
+        .| CL.catMaybes
+        .| CL.concat
+        .| CC.sinkList
 
 parseParam :: Value -> Maybe Text
 parseParam (Object o) =
